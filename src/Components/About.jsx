dc14abcbe3da781985b432b32dce6401a614097c
Apil Basnet <apilbasnet90@gmail.com>
2023-06-14 07:11:59 +0545

    b

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -5,9 +5,9 @@ const About = () => {
         <>
             <div className='my-40 mx-20 flex'>
                 <div className=' w-[50%] font-semibold  '>
-                    <h1 className='text-4xl font-bold flex mb-20'>
-                       <p>About </p>  
-                         <p className='text-purple-500'>  Hotel Lunar</p>
+                    <h1 className='text-4xl font-bold  mb-20'>
+                       About 
+                         <span className='text-purple-500'>  Hotel Lunar</span>
                     </h1>
                     <p className='mb-10'>
                     Ladies and gentlemen, history keeps repeating itself but doesn't teach us any lessons. 'Never again' has turned into 'again and again and again.' So today, ladies and gentlemen, take Hotel Lunar as a wake-up call and a message to be our messenger that people are the ones who can change what they want to change. 
